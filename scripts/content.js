e4027276b5a60021e3f2721c1e7b81b2476aded1
Kim Minh Thang <thangved02@gmail.com>
2022-12-03 17:38:00 +0700

    Prettier

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,8 @@
-document.querySelector('#login-sv > tbody > tr:nth-child(3)')?.remove();
+document.querySelector("#login-sv > tbody > tr:nth-child(3)")?.remove();
 
-if (document.querySelector('#table-main > tbody > tr > td:nth-child(1) > form'))
-	document.querySelector(
-		'#table-main > tbody > tr > td:nth-child(1) > form',
-	).action = '//qldt.ctu.edu.vn/htql/sinhvien/dang_nhap.php';
+if (document.querySelector("#table-main > tbody > tr > td:nth-child(1) > form"))
+	document.querySelector("#table-main > tbody > tr > td:nth-child(1) > form").action =
+		"//qldt.ctu.edu.vn/htql/sinhvien/dang_nhap.php";
 
-if (document.querySelector('.bg-module-top'))
-	document.querySelector('.bg-module-top').innerHTML = '<h2>Đăng nhập</h2>';
-
+if (document.querySelector(".bg-module-top"))
+	document.querySelector(".bg-module-top").innerHTML = "<h2>Đăng nhập</h2>";
