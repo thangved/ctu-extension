dbaa161abec09f520b1f16d9b57166c20550b09c
Kim Minh Thang <thangved02@gmail.com>
2022-12-03 17:37:17 +0700

    Init project

---
+++ scripts/content.js
@@ -0,0 +1,10 @@
+document.querySelector('#login-sv > tbody > tr:nth-child(3)')?.remove();
+
+if (document.querySelector('#table-main > tbody > tr > td:nth-child(1) > form'))
+	document.querySelector(
+		'#table-main > tbody > tr > td:nth-child(1) > form',
+	).action = '//qldt.ctu.edu.vn/htql/sinhvien/dang_nhap.php';
+
+if (document.querySelector('.bg-module-top'))
+	document.querySelector('.bg-module-top').innerHTML = '<h2>Đăng nhập</h2>';
+
