99e1d9f1520114d4d0a8b467d056b7342863792a
Kim Minh Thang <thangved02@gmail.com>
2022-12-05 14:02:15 +0700

    Fix

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,5 @@
+document.querySelector("#login-sv > tbody > tr:nth-child(3)")?.remove();
+
 if (document.querySelector("#table-main > tbody > tr > td:nth-child(1) > form"))
 	document.querySelector("#table-main > tbody > tr > td:nth-child(1) > form").action =
 		"//qldt.ctu.edu.vn/htql/sinhvien/dang_nhap.php";
