1d3362b518b8172cb5efa0bf031a2f5fa1772cc0
Kim Minh Thang <thangved02@gmail.com>
2024-08-10 19:23:28 +0700

    chore: Refactor StudyPlan and Timetable components

---
+++ src/features/Timetable/Footer.tsx
@@ -0,0 +1,35 @@
+import { InfoCircleTwoTone } from '@ant-design/icons';
+import { AutoComplete, Popover, Space, Typography } from 'antd';
+import { DefaultOptionType } from 'antd/es/select';
+
+export type FooterProps = {
+	maxLength: number;
+	setMaxLength: (value: number) => void;
+	lengthOptions: DefaultOptionType[];
+};
+
+export default function Footer({
+	lengthOptions,
+	maxLength,
+	setMaxLength,
+}: FooterProps) {
+	return (
+		<Space>
+			<Typography.Text>Giới hạn tính</Typography.Text>
+			<AutoComplete
+				placeholder="Giới hạn tính"
+				value={maxLength}
+				style={{ width: 200 }}
+				options={lengthOptions}
+				onSelect={(value) => setMaxLength(value)}
+			/>
+
+			<Popover
+				title="Lưu ý"
+				content="Giới hạn quá cao có thể gây ra tình trạng tràn bộ nhớ"
+			>
+				<InfoCircleTwoTone />
+			</Popover>
+		</Space>
+	);
+}
